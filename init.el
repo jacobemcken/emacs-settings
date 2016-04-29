00d5de00834a9bfcc11deb79847e41c09d513058
Jacob Emcken <je@casalogic.dk>
2016-04-29 14:44:50 +0200

    Disable now unused (unstable) package repo
    This is currently only needed for expand-region.

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
